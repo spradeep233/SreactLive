ac248bcd42230958d52b3a1b0c61b245138d0067
pradeep sharma <pradeep160893@outlook.com>
2020-09-14 02:13:02 +0530

    Updates

---
+++ precache-manifest.e3f5374743d0a8afac0e00e7f1d00f2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffb2a752e80295c0e2ad0d7ed2676357",
+    "url": "/SreactLive/index.html"
+  },
+  {
+    "revision": "b92c5c7f3d8a2da8ee14",
+    "url": "/SreactLive/static/css/main.d562d355.chunk.css"
+  },
+  {
+    "revision": "73a89091aa37468ba845",
+    "url": "/SreactLive/static/js/2.123de532.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SreactLive/static/js/2.123de532.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b92c5c7f3d8a2da8ee14",
+    "url": "/SreactLive/static/js/main.625a639d.chunk.js"
+  },
+  {
+    "revision": "be0b12ca3281b7d9919a",
+    "url": "/SreactLive/static/js/runtime-main.c4ab8d02.js"
+  }
+]);
